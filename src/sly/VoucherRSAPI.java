3805fd12a0e4903eb35273e48ad4fe41d2bd82a2
Sylvester Mwambeke <sylvestermwambeke@gmail.com>
2019-06-13 17:51:09 +0300

    Minor changes

--- src/sly/VoucherRSAPI.java
+++ src/sly/VoucherRSAPI.java
@@ -15,6 +15,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -22,21 +24,23 @@ import org.json.JSONObject;
 @Path("/VoucherRSAPI")
 public class VoucherRSAPI {
  
+	@Path("{amount}")
 	@POST
 	@Produces("application/json")
-	public Response createVoucher() throws JSONException {
+	public Response createVoucher(@PathParam("amount") int amount) throws JSONException {
 		
 		String voucherNumber = formatDigits(generateUniqueVoucherCode(16, 4), 4);
 		
 		DatabaseConnector databaseConnector = new DatabaseConnector();
 		Connection conn = databaseConnector.getConn();
 		
-		String insertSQL_voucher  = "INSERT INTO vouchers(voucher_num) "
-				+ "values(?)";
+		String insertSQL_voucher  = "INSERT INTO vouchers(voucher_num,expiry_date, amount) "
+				+ "values(?,NOW() + INTERVAL 5 MINUTE,?)";
 		PreparedStatement preparedStatement_voucher  ;
 		try {
 			preparedStatement_voucher   = conn.prepareStatement(insertSQL_voucher);
-			preparedStatement_voucher.setString(1, voucherNumber);						
+			preparedStatement_voucher.setString(1, voucherNumber);	
+			preparedStatement_voucher.setInt(2, amount);
 									
 			preparedStatement_voucher.executeUpdate();		
 			
@@ -66,15 +70,17 @@ public class VoucherRSAPI {
 		}
  
 		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("created", true);
 		jsonObject.put("voucherNumber", voucherNumber);
  
 		String result = jsonObject.toString();
 		return Response.status(200).entity(result).build();
 	}
 	
+	@Path("{duration}")
 	@GET
 	@Produces("application/json")
-	public Response voucherNoLastThreeMinutes() throws JSONException {
+	public Response voucherNumLastMinutes(@PathParam("duration") int duration) throws JSONException {
 		
 		DatabaseConnector databaseConnector = new DatabaseConnector();
 		Connection conn = databaseConnector.getConn();
@@ -82,7 +88,12 @@ public class VoucherRSAPI {
 		 String selectVoucher = "SELECT "
 					+ "voucher_num "
 					+ "FROM vouchers "
-					+ "WHERE created_at > NOW()- INTERVAL 3 MINUTE ";
+					+ "WHERE created_at > NOW()- INTERVAL ? MINUTE ";
+		 
+		 System.out.println(selectVoucher);
+		 
+		 JSONArray jsonArray = new JSONArray();
+			
 		 
 		    PreparedStatement pstmtVoucher = null;
 	        ResultSet rsVoucher = null;
@@ -90,11 +101,14 @@ public class VoucherRSAPI {
 			
 			try {
 				pstmtVoucher = conn.prepareStatement(selectVoucher);
+				pstmtVoucher.setInt(1, duration);
 				rsVoucher = pstmtVoucher.executeQuery();	
 		       
 		        while (rsVoucher.next()) {
+		        	JSONObject jsonObject = new JSONObject();
 		        	voucherNumber = rsVoucher.getString("voucher_num");
-		        	
+		        	jsonObject.put("voucherNumber", voucherNumber);
+		        	jsonArray.put(jsonObject);
 		        }
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -102,19 +116,18 @@ public class VoucherRSAPI {
 				
 			}	
  
-		JSONObject jsonObject = new JSONObject();
-		jsonObject.put("voucherNumber", voucherNumber);
+		
 		
  
-		String result = jsonObject.toString();
+		String result = jsonArray.toString();
 		return Response.status(200).entity(result).build();
 	}
 	
  
-	@Path("{sNo}")
+	@Path("{s}/{sNo}")
 	@GET
 	@Produces("application/json")
-	public Response voucherNoBySerialNo(@PathParam("sNo") String sNo) throws JSONException {
+	public Response voucherNoBySerialNo(@PathParam("sNo") String s, @PathParam("sNo") String sNo) throws JSONException {
 		
 		DatabaseConnector databaseConnector = new DatabaseConnector();
 		Connection conn = databaseConnector.getConn();
@@ -122,7 +135,9 @@ public class VoucherRSAPI {
 		 String selectVoucher = "SELECT "
 					+ "voucher_num "
 					+ "FROM vouchers "
-					+ "WHERE serial_num > ? ";
+					+ "WHERE serial_num = ? ";
+		 
+		 System.out.println(selectVoucher +", sNo: "+sNo);
 		 
 		    PreparedStatement pstmtVoucher = null;
 	        ResultSet rsVoucher = null;
@@ -160,8 +175,8 @@ public class VoucherRSAPI {
 		
 		 String updateVoucher = "UPDATE "
 					+ "vouchers "
-					+ "SET is_active = ? "
-					+ "WHERE is_active = ? "
+					+ "SET status = ? "
+					+ "WHERE status = ? "
 					+ "AND created_at > NOW()- INTERVAL 5 MINUTE";
 		 
 		    PreparedStatement pstmtVoucher = null;
@@ -194,8 +209,13 @@ public class VoucherRSAPI {
 		DatabaseConnector databaseConnector = new DatabaseConnector();
 		Connection conn = databaseConnector.getConn();
 		
-		 String deleteVoucher = "DELETE "
+		 /*String deleteVoucher = "DELETE "
 					+ "FROM vouchers "
+					+ "WHERE is_active = ?"; */
+		 
+		 String deleteVoucher = "UPDATE "
+					+ "vouchers "
+					+ "SET is_deleted = 1 "
 					+ "WHERE is_active = ?";
 		 
 		    PreparedStatement pstmtVoucher = null;
@@ -231,7 +251,7 @@ public class VoucherRSAPI {
 		
 		 String updateVoucher = "UPDATE "
 					+ "vouchers "
-					+ "SET is_taken = ? "
+					+ "SET status = ? "
 					+ "WHERE voucher_num = ? ";
 		 
 		    PreparedStatement pstmtVoucher = null;
@@ -241,7 +261,7 @@ public class VoucherRSAPI {
 			
 			try {
 				pstmtVoucher = conn.prepareStatement(updateVoucher);
-				pstmtVoucher.setInt(1, 0);
+				pstmtVoucher.setInt(1, 2);
 				pstmtVoucher.setString(2, vNo);
 				pstmtVoucher.executeUpdate();
 				jsonObject.put("vNo", vNo);
@@ -262,6 +282,8 @@ public class VoucherRSAPI {
 	    long nano = System.nanoTime();
 	    Random rand = new Random();
 	    StringBuilder util = new StringBuilder(String.valueOf(nano));
+	    System.out.println("String.valueOf(nano): "+String.valueOf(nano));
+	    System.out.println("Hapa: "+util.toString());
 	    util = util.reverse();
 	   
 	    System.out.println("Hapa: "+util.toString());
